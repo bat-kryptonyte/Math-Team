cced38741b97a0de348d6ec320699df0b6a1cda8
Jeffrey Deng <jeffreytheflash123@gmail.com>
2020-07-22 13:21:08 -0700

    Updates

---
+++ precache-manifest.5f078f2657bcd8043da1c73c80172667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5206f7cfd3d5f875e4b",
+    "url": "/Cornell-Club/static/css/main.9ddf93c1.chunk.css"
+  },
+  {
+    "revision": "c5206f7cfd3d5f875e4b",
+    "url": "/Cornell-Club/static/js/main.c5206f7c.chunk.js"
+  },
+  {
+    "revision": "5af9c9776e2d211f9899",
+    "url": "/Cornell-Club/static/css/1.e498baf9.chunk.css"
+  },
+  {
+    "revision": "5af9c9776e2d211f9899",
+    "url": "/Cornell-Club/static/js/1.5af9c977.chunk.js"
+  },
+  {
+    "revision": "ef6e6df11ed79fb5783d",
+    "url": "/Cornell-Club/static/js/runtime~main.ef6e6df1.js"
+  },
+  {
+    "revision": "6dabbc3eebfe31555b4bdf3e4a682b1e",
+    "url": "/Cornell-Club/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Cornell-Club/precache-manifest.5f078f2657bcd8043da1c73c80172667.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Cornell-Club/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
