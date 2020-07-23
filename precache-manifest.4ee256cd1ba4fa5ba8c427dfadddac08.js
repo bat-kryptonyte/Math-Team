d26e282507ad654c10b93c018d0a7f39140e5afb
Jeffrey Deng <jeffreytheflash123@gmail.com>
2020-07-23 00:20:15 -0700

    Updates

--- precache-manifest.4ee256cd1ba4fa5ba8c427dfadddac08.js
+++ precache-manifest.4ee256cd1ba4fa5ba8c427dfadddac08.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "24a7df1a190e2116b4b7",
+    "revision": "d95501719fc0267ec56a",
     "url": "/Math-Team/static/css/main.68ce7322.chunk.css"
   },
   {
-    "revision": "24a7df1a190e2116b4b7",
-    "url": "/Math-Team/static/js/main.24a7df1a.chunk.js"
+    "revision": "d95501719fc0267ec56a",
+    "url": "/Math-Team/static/js/main.d9550171.chunk.js"
   },
   {
     "revision": "07ddfcf9b15537b01642",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/Math-Team/static/js/runtime~main.849e1faa.js"
   },
   {
-    "revision": "01de6c891f0769ecc380fe8aa3adc4ef",
+    "revision": "325deb96d6b55d1352e95b57464d3e38",
     "url": "/Math-Team/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Math-Team/precache-manifest.adcef23eda982f34c75d32d0183fc34c.js"
+  "/Math-Team/precache-manifest.4ee256cd1ba4fa5ba8c427dfadddac08.js"
 );
 
 workbox.clientsClaim();
