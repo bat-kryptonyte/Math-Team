49f8be391e6e2384c9c0a0edfce2b14b91c616cb
Jeffrey Deng <jeffreytheflash123@gmail.com>
2020-08-17 15:40:26 -0700

    Updates

---
+++ precache-manifest.2db5fa0a71c8367f86bf0a9f2d9954f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bea11040da32b7b7c52",
+    "url": "/static/css/main.68ce7322.chunk.css"
+  },
+  {
+    "revision": "9bea11040da32b7b7c52",
+    "url": "/static/js/main.9bea1104.chunk.js"
+  },
+  {
+    "revision": "0f322079f69c8abd6c74",
+    "url": "/static/css/1.c9f401cf.chunk.css"
+  },
+  {
+    "revision": "0f322079f69c8abd6c74",
+    "url": "/static/js/1.0f322079.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3302d5152efc73587d4589b24c2ac88e",
+    "url": "/index.html"
+  }
+];
